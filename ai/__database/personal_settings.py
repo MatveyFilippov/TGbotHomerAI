906923146d2e28a38345180f77cce1d63151b7f4
MatveyFilippov <matdufi2004@gmail.com>
2025-01-16 22:47:54 +0300

    bug fix in first time calling

--- ai/__database/personal_settings.py
+++ ai/__database/personal_settings.py
@@ -6,7 +6,7 @@ def get_personal_settings(user_tg_peer_id: int) -> Type[PersonalSettings]:
     with Session() as session:
         personal_settings = session.query(PersonalSettings).get(user_tg_peer_id)
         if not personal_settings:
-            personal_settings = PersonalSettings(tg_peer_id=user_tg_peer_id)
-            session.add(personal_settings)
+            session.add(PersonalSettings(tg_peer_id=user_tg_peer_id))
             session.commit()
-        return personal_settings
+            personal_settings = session.query(PersonalSettings).get(user_tg_peer_id)
+    return personal_settings
